ed27692ce73f01f1719e3af1ac9929ebbea2e028
VadimTrubay <vadnetvadnet@ukr.net>
2024-07-03 09:58:25 +0300

    init commit

---
+++ src/components/App.tsx
@@ -0,0 +1,18 @@
+import reactLogo from "../assets/react.svg"
+import viteLogo from "../assets/vite.svg"
+import "./App.css"
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Vite React App!</h1>
+        <img src={reactLogo} alt="React Logo" className="App-logo" /> +
+        <img src={viteLogo} alt="Vite Logo" className="App-logo" />
+      </header>
+    </div>
+  )
+}
+
+export default App
