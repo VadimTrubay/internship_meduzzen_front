25fee67cfb97c466dda15ac4c2ee00f5472ad8b2
VadimTrubay <vadnetvadnet@ukr.net>
2024-07-14 10:53:26 +0300

    finally commit and end i`m speaking

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "modern-normalize";
+import {Provider} from "react-redux";
 import ReactDOM from "react-dom/client";
 import {store, persistor} from "./redux/store";
 import {BrowserRouter} from "react-router-dom";
 import {HelmetProvider} from "react-helmet-async";
 import {PersistGate} from "redux-persist/integration/react";
-import {Provider} from "react-redux";
 import App from "./components/App";
 import Auth0ProviderWithNavigate from "./components/Auth0ProviderWithNavigate/Auth0ProviderWithNavigate";
 import "./index.css";
